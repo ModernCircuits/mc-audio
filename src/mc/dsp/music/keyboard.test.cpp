3c564cdc167ca0e7d74b6687bc77979c28dd37b7
Tobias Hienzsch <tobias@modern-circuits.com>
2022-09-03 11:56:31 +0200

    [music] Improve isBlackKey/isWhiteKey tests

--- src/mc/dsp/music/keyboard.test.cpp
+++ src/mc/dsp/music/keyboard.test.cpp
@@ -3,39 +3,49 @@
 #include <mc/dsp/music.hpp>
 
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
 
 namespace dsp = mc::dsp;
 
+static auto makeNote(int octave, int note) -> dsp::Note
+{
+    return dsp::Note{static_cast<uint8_t>(octave * 12 + note)};
+}
+
 TEST_CASE("dsp/music: isBlackKey", "[dsp][music]")
 {
-    REQUIRE(dsp::isBlackKey(dsp::Note{25}));  // C#1/Db1
-    REQUIRE(dsp::isBlackKey(dsp::Note{27}));  // D#1/Eb1
-    REQUIRE(dsp::isBlackKey(dsp::Note{27}));  // D#1/Eb1
-    REQUIRE(dsp::isBlackKey(dsp::Note{30}));  // F#1/Gb1
-    REQUIRE(dsp::isBlackKey(dsp::Note{32}));  // G#1/Ab1
-    REQUIRE(dsp::isBlackKey(dsp::Note{34}));  // A#1/Bb1
+    auto octave = GENERATE(0, 1, 2, 3, 4, 5, 6, 7);
 
-    REQUIRE_FALSE(dsp::isBlackKey(dsp::Note{24}));  // C1
-    REQUIRE_FALSE(dsp::isBlackKey(dsp::Note{26}));  // D1
-    REQUIRE_FALSE(dsp::isBlackKey(dsp::Note{28}));  // E1
-    REQUIRE_FALSE(dsp::isBlackKey(dsp::Note{29}));  // F1
-    REQUIRE_FALSE(dsp::isBlackKey(dsp::Note{31}));  // G1
-    REQUIRE_FALSE(dsp::isBlackKey(dsp::Note{33}));  // A1
+    REQUIRE(dsp::isBlackKey(makeNote(octave, 1)));   // C#/Db
+    REQUIRE(dsp::isBlackKey(makeNote(octave, 3)));   // D#/Eb
+    REQUIRE(dsp::isBlackKey(makeNote(octave, 6)));   // F#/Gb
+    REQUIRE(dsp::isBlackKey(makeNote(octave, 8)));   // G#/Ab
+    REQUIRE(dsp::isBlackKey(makeNote(octave, 10)));  // A#/Bb
+
+    REQUIRE_FALSE(dsp::isBlackKey(makeNote(octave, 0)));   // C
+    REQUIRE_FALSE(dsp::isBlackKey(makeNote(octave, 2)));   // D
+    REQUIRE_FALSE(dsp::isBlackKey(makeNote(octave, 4)));   // E
+    REQUIRE_FALSE(dsp::isBlackKey(makeNote(octave, 5)));   // F
+    REQUIRE_FALSE(dsp::isBlackKey(makeNote(octave, 7)));   // G
+    REQUIRE_FALSE(dsp::isBlackKey(makeNote(octave, 9)));   // A
+    REQUIRE_FALSE(dsp::isBlackKey(makeNote(octave, 11)));  // B
 }
 
 TEST_CASE("dsp/music: isWhiteKey", "[dsp][music]")
 {
-    REQUIRE(dsp::isWhiteKey(dsp::Note{24}));  // C1
-    REQUIRE(dsp::isWhiteKey(dsp::Note{26}));  // D1
-    REQUIRE(dsp::isWhiteKey(dsp::Note{28}));  // E1
-    REQUIRE(dsp::isWhiteKey(dsp::Note{29}));  // F1
-    REQUIRE(dsp::isWhiteKey(dsp::Note{31}));  // G1
-    REQUIRE(dsp::isWhiteKey(dsp::Note{33}));  // A1
+    auto octave = GENERATE(0, 1, 2, 3, 4, 5, 6, 7);
 
-    REQUIRE_FALSE(dsp::isWhiteKey(dsp::Note{25}));  // C#1/Db1
-    REQUIRE_FALSE(dsp::isWhiteKey(dsp::Note{27}));  // D#1/Eb1
-    REQUIRE_FALSE(dsp::isWhiteKey(dsp::Note{27}));  // D#1/Eb1
-    REQUIRE_FALSE(dsp::isWhiteKey(dsp::Note{30}));  // F#1/Gb1
-    REQUIRE_FALSE(dsp::isWhiteKey(dsp::Note{32}));  // G#1/Ab1
-    REQUIRE_FALSE(dsp::isWhiteKey(dsp::Note{34}));  // A#1/Bb1
+    REQUIRE(dsp::isWhiteKey(makeNote(octave, 0)));   // C
+    REQUIRE(dsp::isWhiteKey(makeNote(octave, 2)));   // D
+    REQUIRE(dsp::isWhiteKey(makeNote(octave, 4)));   // E
+    REQUIRE(dsp::isWhiteKey(makeNote(octave, 5)));   // F
+    REQUIRE(dsp::isWhiteKey(makeNote(octave, 7)));   // G
+    REQUIRE(dsp::isWhiteKey(makeNote(octave, 9)));   // A
+    REQUIRE(dsp::isWhiteKey(makeNote(octave, 11)));  // B
+
+    REQUIRE_FALSE(dsp::isWhiteKey(makeNote(octave, 1)));   // C#/Db
+    REQUIRE_FALSE(dsp::isWhiteKey(makeNote(octave, 3)));   // D#/Eb
+    REQUIRE_FALSE(dsp::isWhiteKey(makeNote(octave, 6)));   // F#/Gb
+    REQUIRE_FALSE(dsp::isWhiteKey(makeNote(octave, 8)));   // G#/Ab
+    REQUIRE_FALSE(dsp::isWhiteKey(makeNote(octave, 10)));  // A#/Bb
 }
