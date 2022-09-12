51e0cf5192ded4511e5fb25007f2ba12b7e8534c
Tobias Hienzsch <tobias@modern-circuits.com>
2022-09-12 22:59:50 +0200

    [units] Add constexpr tests to Decibel

--- src/mc/dsp/units/decibel.test.cpp
+++ src/mc/dsp/units/decibel.test.cpp
@@ -7,9 +7,14 @@
 
 namespace dsp = mc::dsp;
 
+template<typename T>
+static constexpr auto zeroDB = dsp::Decibel<T>{0};
+
 TEMPLATE_TEST_CASE("dsp/units: toDecibel", "[dsp][units]", float, double)
 {
     using T = TestType;
+    STATIC_REQUIRE(dsp::toDecibel(T(1.0)).count() == T(0));
+
     REQUIRE(dsp::toDecibel(T(1.0)).count() == Catch::Approx(T(0)));
     REQUIRE(dsp::toDecibel(T(0.5)).count() == Catch::Approx(T(-6.02059)));
     REQUIRE(dsp::toDecibel(T(0.25)).count() == Catch::Approx(T(-12.04119)));
@@ -18,7 +23,10 @@ TEMPLATE_TEST_CASE("dsp/units: toDecibel", "[dsp][units]", float, double)
 TEMPLATE_TEST_CASE("dsp/units: toGain", "[dsp][units]", float, double)
 {
     using T = TestType;
-    REQUIRE(dsp::toGain(dsp::Decibel{T(0)}) == Catch::Approx(T(1.0)));
+
+    STATIC_REQUIRE(dsp::toGain(zeroDB<T>) == T(1));
+
+    REQUIRE(dsp::toGain(zeroDB<T>) == Catch::Approx(T(1.0)));
     REQUIRE(dsp::toGain(dsp::Decibel{T(-6.02059)}) == Catch::Approx(T(0.5)));
     REQUIRE(dsp::toGain(dsp::Decibel{T(-12.04119)}) == Catch::Approx(T(0.25)));
 }
@@ -26,10 +34,10 @@ TEMPLATE_TEST_CASE("dsp/units: toGain", "[dsp][units]", float, double)
 TEMPLATE_TEST_CASE("dsp/units: operator==(Decibel,Decibel)", "[dsp][units]", float, double)
 {
     using T = TestType;
-    REQUIRE(dsp::Decibel{T(0)} == dsp::Decibel{T(0)});
-    REQUIRE(dsp::Decibel{T(0)} != dsp::Decibel{T(1)});
-    REQUIRE(dsp::Decibel{T(0)} < dsp::Decibel{T(1)});
-    REQUIRE(dsp::Decibel{T(0)} <= dsp::Decibel{T(1)});
-    REQUIRE_FALSE(dsp::Decibel{T(0)} > dsp::Decibel{T(1)});
-    REQUIRE_FALSE(dsp::Decibel{T(0)} >= dsp::Decibel{T(1)});
+    STATIC_REQUIRE(zeroDB<T> == zeroDB<T>);
+    STATIC_REQUIRE(zeroDB<T> != dsp::Decibel{T(1)});
+    STATIC_REQUIRE(zeroDB<T> < dsp::Decibel{T(1)});
+    STATIC_REQUIRE(zeroDB<T> <= dsp::Decibel{T(1)});
+    STATIC_REQUIRE_FALSE(zeroDB<T> > dsp::Decibel{T(1)});
+    STATIC_REQUIRE_FALSE(zeroDB<T> >= dsp::Decibel{T(1)});
 }
