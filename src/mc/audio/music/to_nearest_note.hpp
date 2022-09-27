60dfd8c8118c35aad502cead3374ccb364a93981
Tobias Hienzsch <tobias@modern-circuits.com>
2022-09-28 00:30:19 +0200

    [music] Remove toNearestNote Span overload

--- src/mc/audio/music/to_nearest_note.hpp
+++ src/mc/audio/music/to_nearest_note.hpp
@@ -20,12 +20,4 @@ auto toNearestNote(Hertz<T> frequency) -> Note
     return Note{static_cast<std::uint8_t>(std::round(raw))};
 }
 
-/// \brief Get MIDI note numbers for given frequencies
-/// \ingroup mc-audio-music
-template<typename T>
-auto toNearestNote(Span<Frequency<T> const> freq, Span<Note> out) -> void
-{
-    std::transform(begin(freq), end(freq), begin(out), toNearestNote<T>);
-}
-
 }  // namespace mc
