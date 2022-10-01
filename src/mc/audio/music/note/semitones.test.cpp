a88e7dedef6b33259aad65b284b6cac34ea0676e
Tobias Hienzsch <tobias@modern-circuits.com>
2022-10-02 00:27:43 +0200

    [music] Add tests to Semitones

---
+++ src/mc/audio/music/note/semitones.test.cpp
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: BSL-1.0
+
+#include <mc/audio/music/note.hpp>
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("music/note: compare(Semitones)", "[music]")
+{
+    using namespace mc;
+    REQUIRE(Semitones{1} == Semitones{1});
+    REQUIRE(Semitones{1} != Semitones{2});
+    REQUIRE(Semitones{1} < Semitones{2});
+    REQUIRE(Semitones{1} <= Semitones{2});
+    REQUIRE_FALSE(Semitones{1} > Semitones{2});
+    REQUIRE_FALSE(Semitones{1} >= Semitones{2});
+}
+
+TEST_CASE("music/note: operator++(Semitones)", "[music]")
+{
+    using namespace mc;
+
+    auto note = Semitones{1};
+    REQUIRE(note == Semitones{1});
+
+    ++note;
+    REQUIRE(note == Semitones{2});
+
+    note++;
+    REQUIRE(note == Semitones{3});
+}
+
+TEST_CASE("music/note: operator--(Semitones)", "[music]")
+{
+    using namespace mc;
+
+    auto note = Semitones{4};
+    REQUIRE(note == Semitones{4});
+
+    --note;
+    REQUIRE(note == Semitones{3});
+
+    note--;
+    REQUIRE(note == Semitones{2});
+}
