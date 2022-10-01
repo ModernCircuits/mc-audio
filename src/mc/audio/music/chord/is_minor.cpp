0bfed810d6962de2383a6a5c80b138b32e279753
Tobias Hienzsch <tobias@modern-circuits.com>
2022-10-02 00:38:04 +0200

    [music] Add isMinor

--- src/mc/audio/music/chord/is_minor.cpp
+++ src/mc/audio/music/chord/is_minor.cpp
@@ -3,11 +3,18 @@
 
 #include <mc/core/config.hpp>
 
-#include <mc/core/array.hpp>
-#include <mc/core/type_traits.hpp>
+#include <mc/audio/music/note/semitones.hpp>
 
 namespace mc {
 
-constexpr auto isMinor(Span<Note const> notes) noexcept -> bool { return notes.empty(); }
+constexpr auto isMinor(Span<Note const> notes) noexcept -> bool
+{
+    if (notes.size() < 2) { return false; }
+    if (notes.size() == 2) {
+        if (notes[1] - notes[0] == Semitones{3}) { return true; }
+    }
+
+    return notes[1] - notes[0] == Semitones{3} && notes[2] - notes[0] == Semitones{7};
+}
 
 }  // namespace mc
--- src/mc/audio/music/chord/is_minor.test.cpp
+++ src/mc/audio/music/chord/is_minor.test.cpp
@@ -2,10 +2,17 @@
 
 #include <mc/audio/music/chord.hpp>
 
+#include <mc/core/array.hpp>
+
 #include <catch2/catch_test_macros.hpp>
 
 TEST_CASE("music/chord: isMinor", "[music]")
 {
     using namespace mc;
-    SUCCEED();
+
+    REQUIRE(isMinor(Array<Note, 3>{Note{60}, Note{63}, Note{67}}));
+
+    REQUIRE_FALSE(isMinor({}));
+    REQUIRE_FALSE(isMinor(Array<Note, 3>{}));
+    REQUIRE_FALSE(isMinor(Array<Note, 3>{Note{60}, Note{60}, Note{60}}));
 }
